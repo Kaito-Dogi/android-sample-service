630d533827801cf2b65e65052ecf8e4429f73ac5
Kaito-Dogi <kendobu0405@gmail.com>
2021-12-16 00:06:07 +0900

    add: ServiceにMediaPlayerを追加し，アプリのライフサイクルを確認

--- app/src/main/java/app/doggy/servicesample/MyService.kt
+++ app/src/main/java/app/doggy/servicesample/MyService.kt
@@ -2,16 +2,30 @@ package app.doggy.servicesample
 
 import android.app.Service
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.IBinder
 import android.util.Log
 
 class MyService : Service() {
+
+    // MediaPlayerの変数
+    private lateinit var sheepBleat: MediaPlayer
+
+    // MediaPlayerのループを切り替える．
+    private var isLooping = true
 
     // サービスが最初に作成された時に呼び出される．
     // onStartCommand()やonBind()の前に呼び出される．
     override fun onCreate() {
         super.onCreate()
         Log.d(SERVICE_LOG, "onCreate() @MyService")
+
+        sheepBleat = MediaPlayer.create(this, R.raw.sheep_bleat)
+        sheepBleat.isLooping = isLooping
+        sheepBleat.setOnCompletionListener {
+            Log.d(SERVICE_LOG, "stopSelf() @MyService")
+            stopSelf()
+        }
     }
 
     // 他のコンポーネント（アクティビティなど）がサービスの開始をリクエストするためにstartService()を呼び出した時に呼び出される．
@@ -21,12 +35,20 @@ class MyService : Service() {
     // ただしAndroid 8.0（APIレベル26, Oreo)以降ではアプリがバックグラウンドに移行して1分ほどでサービスが停止してしまう．
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d(SERVICE_LOG, "onStartCommand() @MyService")
+
+        sheepBleat.start()
+
         return super.onStartCommand(intent, flags, startId)
     }
 
     // サービスが使用されなくなり破棄されるときに呼び出される．
     override fun onDestroy() {
         super.onDestroy()
+
+        sheepBleat.stop()
+        sheepBleat.reset()
+        sheepBleat.release()
+
         Log.d(SERVICE_LOG, "onDestroy() @MyService")
     }
 
